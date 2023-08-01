d2df72d8890631a69b4fb43d1a7155816c43363e
ThimPad <haathimmunas@gmail.com>
2023-08-01 17:51:29 +0530

    Added basic server file with accept, respond and close connection

---
+++ server.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+
+
+#define PORT 18000
+#define BACKLOG_QUEUE_LEN 10
+#define NO_FLAGS 0
+// #define sizeofsockaddr()
+
+int main(int argc, char **argv){
+    
+    // check for arguments are valid
+
+
+    char *response = "HTTP/1.1 200 OK\r\nContent-Length: 16\r\nContent-Type: text/plain\r\n\r\nHello, client!";
+
+    // create listening socket
+    int listeningSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // bind socket to IP
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+    
+    // Bind the socket to the specified IP and port
+    int bindErr = bind(listeningSocket, (struct sockaddr*) &address, (socklen_t) sizeof(address));
+
+    int listenErr = listen(listeningSocket, BACKLOG_QUEUE_LEN);
+
+    while (1){
+
+        // int newSocket = accept(listeningSocket, (struct sockaddr*) &address, (socklen_t*) sizeof(address));
+        int newSocket = accept(listeningSocket, NULL, NULL);
+
+        printf("Accepted a connection..\n");
+
+        // send(newSocket, response, strlen(response), NO_FLAGS);
+        write(newSocket, response, strlen(response));
+        
+        printf("Sent\n");
+
+        close(newSocket);
+    }
+    
+    return 0;
+}
